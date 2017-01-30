b3943c1f75f4a4c1faf4f405bda4555d66a31c74
Jakub Froń <kubafron@op.pl>
2017-01-30 07:45:10 +0000

    add BMI2

---
+++ 170130/BMI2.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class BMI2{
+    public static void main(String[] args){
+        do{
+        int c;
+        Scanner in = new Scanner(System.in);
+        System.out.println("Podaj swoją wagę(bez oszukiwania): ");
+        double waga = in.nextDouble();
+        System.out.println("Podaj swój wzrost: ");
+        double wzrost = in.nextDouble();
+        double BMI = waga/(wzrost*wzrost);
+        if(BMI<18.5) System.out.println("Niedowaga");
+        else if(BMI>=18.5 && BMI <= 24.99) System.out.println("Waga prawidłowa");
+        else System.out.println("Nadwaga");
+        do{
+        System.out.println("Chcesz kontynuować? 1-tak 2-nie");
+            c=in.nextInt();
+        }while(c!=1 && c!=2);
+        }while(c==1);
+    }
+}

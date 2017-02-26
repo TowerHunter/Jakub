e6c0398d65f58c1d242b8d459af71ffe2f3188b7
Jakub Froń <kubafron@op.pl>
2017-02-26 15:08:30 +0000

    update2

--- Ferie/pracadomowa/liczbyinaczej.java
+++ Ferie/pracadomowa/liczbyinaczej.java
@@ -10,20 +10,14 @@ public class liczbyinaczej{
         File file=new File("liczbyinaczej.txt");
         PrintWriter zapis= new PrintWriter("wynik.txt");
         Scanner in = new Scanner(file);
-<<<<<<< HEAD
         String text = in.nextLine();
         int text2 = Integer.parseInt(text, 2);
         int max = text2;
-=======
-        String text;
-        int text2;
->>>>>>> ff3b5709c2a6fbad71389b5ef840e473ff296a34
         int ile = 0;
         int ile2 = 0;
         int suma = text2;
         while(in.hasNextLine())
         {
-<<<<<<< HEAD
             text = in.nextLine();
             text2 = Integer.parseInt(text, 2);
             if(text2>max) max=text2;
@@ -38,18 +32,8 @@ public class liczbyinaczej{
         zapis.println("b)"+max);
         zapis.println("  "+Integer.toBinaryString(max));
         zapis.println("c)"+ile2);
-        zapis.println("  "+suma);
+        zapis.println("  "+Integer.toBinaryString(suma));
         zapis.close();
-
-
-=======
-            text=in.nextLine();
-            System.out.println(text);
-        }
-        zapis.close();
->>>>>>> ff3b5709c2a6fbad71389b5ef840e473ff296a34
-        }
-        
-        
    
     }
+}
